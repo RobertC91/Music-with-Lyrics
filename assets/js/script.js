b02e83cd750614a71ab52b4178b1f93d0acf691f
RobertC91 <134195467+RobertC91@users.noreply.github.com>
2023-07-13 21:14:34 -0500

    Added button function and Lyric API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,29 @@
 const audioPlayer = document.getElementById("audioPlayer");
 const lyricsContainer = document.getElementById("lyricsContainer");
-const localStorageKey = "selectedSong";
-const searchQuery = "Caught in a Hustle";
+const searchedSongs = document.getElementById("storedSongText");
+const searchInput = document.getElementById('searchInput')
+const artistQuery = "Immortal Technique"
 const searchButton = document.getElementById("searchButton")
 const musicAPIKey = "0996a1c4acbf700517ecfedf926ddcde"
 const lyricsAPIKey = "edb60ff7bf4e024322127eafa296dd31"
 
-// Define your API endpoints for retrieving lyrics and songs
-const lyricsAPI = "https://api.musixmatch.com/ws/1.1/artist.search?q_artist=prodigy&page_size=5&apikey=" + lyricsAPIKey;
-const songsAPI = "https://ws.audioscrobbler.com/2.0/?method=track.search&track=" + searchQuery + "&api_key=" + musicAPIKey + "&format=json"
-;
+
+
 
 // Fetch lyrics for a given song and display them
 function fetchLyrics() {
+  const songQuery = searchInput.value
+  console.log(songQuery)
 $.ajax({
   type:"GET",
   data: {
     apikey: "edb60ff7bf4e024322127eafa296dd31",
-    q_artist: "Nickelback",
+    q_track: songQuery,
+    q_artist: artistQuery,
     format: "jsonp",
     callback: "jsonp_callback"
   },
-  url: "https://api.musixmatch.com/ws/1.1/artist.search",
+  url: "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get",
   dataType: "jsonp",
   jsonpCallback: "jsonp_callback",
   contentType: "application/json",
@@ -32,74 +34,36 @@ $.ajax({
     console.log(jqHXR)
   }
 })
-  // fetch(lyricsAPI)
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     console.log(data)
-  //     if (data.lyrics) {
-  //       lyricsContainer.innerText = data.lyrics;
-  //     } else {
-  //       lyricsContainer.innerText = "Lyrics not found for this song.";
-  //     }
-  //   })
-  //   .catch((error) => {
-  //     console.error("Error:", error);
-  //     lyricsContainer.innerText = "Failed to fetch lyrics.";
-  //   });
+.then(function(data) {
+  // grab lyrics and append them to the page
+  let lyricData = data.message.body.lyrics.lyrics_body;
+  lyricsContainer.append(lyricData)
+})
 }
-fetchLyrics()
-// // Fetch a list of songs from your music API and populate the player
-// function fetchSongs() {   
-//   fetch(songsAPI)  
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log(data) 
-//       for((track) => {
-//         const option = document.createElement("option");
-//         option.text = `${track.artist} - ${track.name}`;
-//         option.value = track.fileURL;
-//         audioPlayer.appendChild(option);
-//       })
-// })
- 
 
-      // Retrieve the selected song from local storage and set it as the default selection
-//       const selectedSong = localStorage.getItem(localStorageKey);
-//       if (selectedSong) {
-//         audioPlayer.value = selectedSong;
-//         const [artist, title] = selectedSong.split(" - ");
-//         fetchLyrics(artist, title);
-//       }
 
-//   // .catch((error) => {
-//   //     console.error("Error:", error);
-//   //     lyricsContainer.innerText = "Failed to fetch songs.";
-//   // })
-// }
-
-// Add an event listener to the search button
-searchButton.addEventListener("click", () => {
-  // Get the search query from the input field
-  const query = searchInput.value;
-
-  // Call the fetchSongs function with the search query
-  fetchSongs(query);
-});
 
 // Event listener for when a song is selected from the dropdown
 audioPlayer.addEventListener("change", (event) => {
-  const selectedSong = event.target.value;
-  // Load the selected song
-  audioPlayer.src = selectedSong;
+const selectedSong = event.target.value;
+// Load the selected song
+audioPlayer.src = selectedSong;
 
-  // Extract the artist and title from the selected song
-  const [artist, title] = selectedSong.split(" - ");
-  // Fetch the lyrics for the selected song
-  fetchLyrics(artist, title);
-
-  // Save the selected song to local storage
-  localStorage.setItem(localStorageKey, selectedSong);
+// Save the selected song to local storage
+localStorage.setItem(localStorageKey, selectedSong);
 });
+ 
+searchButton.addEventListener('click', function() {
+    // take input information and send it to API
+    const inputValue = searchInput.value    
+    localStorage.setItem('searchInputValue', inputValue)
+    console.log(inputValue)
+ })
 
-// Fetch songs when the page loads
-// fetchSongs();
+ const savedValue = localStorage.getItem('searchInputValue');
+ 
+ searchedSongs.textContent = savedValue;
+
+ fetchLyrics()
+
+
